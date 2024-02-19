88802192f177f264ed5e14d861024f22072fab35
MariIto <mari725r@gmail.com>
2024-02-19 07:14:17 +0000

    14-mypage

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  root 'homes#top'
   devise_for :users
-
-  resources :post_images, only: [:new, :create, :index, :show]
-
+  root 'homes#top'
   get "/homes/about" => "homes#about", as: "about"
+
+  resources :post_images, only: [:new, :create, :index, :show, :destroy]
+  resources :users, only: [:show, :edit]
+
 end
